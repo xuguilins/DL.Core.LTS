b4a0a5be07d1d26fb2656ef9faa548f956d37b02
xuguilins <1352249378@qq.com>
2020-10-21 09:24:43 +0800

    增加自动迁移 增加日志

---
+++ DL.Core.ulitity/log/InternalLogger.cs
@@ -0,0 +1,59 @@
+﻿using DL.Core.ulitity.tools;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+namespace DL.Core.ulitity.log
+{
+    /// <summary>
+    /// 系统内置日志
+    /// </summary>
+    public class InternalLogger : LogBase
+    {
+        private static readonly object locker = new object();
+        private static readonly ConcurrentDictionary<string, string> pathdic = new ConcurrentDictionary<string, string>();
+
+        /// <summary>
+        /// 写入日志
+        /// </summary>
+        /// <param name="level">日志级别</param>
+        /// <param name="message">内容</param>
+        /// <param name="exception">异常消息</param>
+        protected override void Write(LogLevel level, object message, Exception exception = null)
+        {
+            lock (locker)
+            {
+                DateTime dateTimeNow = DateTime.Now;
+                string logDirPath = Directory.GetCurrentDirectory()+"\\log";
+                if (!logDirPath.CheckDirctoryIsExite())
+                {
+                    FileExtensition.CreateDic(logDirPath);
+                }
+                string logFilePath = string.Format("{0}\\{1}.log", logDirPath, $"Log_{level.ToString()}_{ dateTimeNow.ToString("yyyy-MM-dd")}");
+                using (StreamWriter writer = new StreamWriter(logFilePath, true, Encoding.UTF8))
+                {
+                    try
+                    {
+                        StackTrace t = new StackTrace();
+                        StackFrame f = t.GetFrame(t.FrameCount - 1);
+                        object source = f.GetMethod();
+                        writer.WriteLine($"[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "]  " + source);
+                        writer.WriteLine(message + "\r\n");
+                        if (exception != null) {
+                            writer.WriteLine($"\r\n{exception.Message}\r\n {exception.StackTrace}");
+                         } 
+                        writer.WriteLine("===============================================================");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    writer.Close();
+                }
+            }
+        }
+    }
+}
