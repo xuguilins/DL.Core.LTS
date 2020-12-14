301c7be26be7142fd693e140ecdd3235e530fb91
xuguilins <1352249378@qq.com>
2020-12-14 22:01:28 +0800

    增加古老的http请求

---
+++ DL.Core.ulitity/web/HttpWebRequestBuild.cs
@@ -0,0 +1,86 @@
+﻿using DL.Core.ulitity.tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace DL.Core.ulitity.web
+{
+    public class HttpWebRequestBuild
+    {
+        public static HttpWebRequestBuild Build => new HttpWebRequestBuild();
+        public string Post(string url,Dictionary<string,object> dic,Dictionary<string,string> header=null,string type= "application/json")=> PostMethod(url, dic, header, type);
+ 
+        public T Post<T>(string url, Dictionary<string, object> dic, Dictionary<string, string> header = null, string type = "application/json")
+        {
+            var data = PostMethod(url, dic, header, type);
+            var obj = data.FromJson<T>();
+            return obj;
+        }
+
+        public T Get<T>(string url, Dictionary<string, string> dic, Dictionary<string, string> header = null, string type = "application/x-www-form-urlencoded")
+        {
+            var data = GetMethod(url, dic, header, type);
+            var obj = data.FromJson<T>();
+            return obj;
+        }
+        public string Get(string url, Dictionary<string, string> dic, Dictionary<string, string> header = null, string type = "application/x-www-form-urlencoded") => GetMethod(url,dic,header,type);
+        private string PostMethod(string url, Dictionary<string, object> dic, Dictionary<string, string> header = null, string type = "application/x-www-form-urlencoded")
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.Method = "POST";
+            request.ContentType = type;
+            var jsondata = dic.ToJson();
+            byte[] bytes = Encoding.UTF8.GetBytes(jsondata);
+            request.ContentLength = bytes.Length;
+            if (header != null && header.Count > 0)
+            {
+                foreach (var item in header.Keys)
+                {
+                    var value = header[item];
+                    request.Headers.Add(item, value);
+                }
+            }
+            Stream stream = request.GetRequestStream();
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Close();
+            var response = request.GetResponse();
+            var resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            var result = reader.ReadToEnd();
+            return result;
+        }
+        private string GetMethod(string url, Dictionary<string, string> dic, Dictionary<string, string> header = null,string type = "application/x-www-form-urlencoded")
+        {
+            StringBuilder sb = new StringBuilder();
+            if (dic != null && dic.Count > 0)
+            {
+                foreach (var item in dic.Keys)
+                {
+                    sb.Append($"{item}={dic[item]}&");
+                }
+            }
+            var requestUrl = url + sb.ToString().ExpenSubstr();
+            HttpWebRequest request = WebRequest.CreateHttp(requestUrl);
+            request.Method = "GET";
+            request.ContentType = type;
+            var jsondata = dic.ToJson();
+            byte[] bytes = Encoding.UTF8.GetBytes(jsondata);
+            request.ContentLength = bytes.Length;
+            if (header != null && header.Count > 0)
+            {
+                foreach (var item in header.Keys)
+                {
+                    var value = header[item];
+                    request.Headers.Add(item, value);
+                }
+            }
+            var response = request.GetResponse();
+            var resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            var result = reader.ReadToEnd();
+            return result;
+        }
+    }
+}
