cdf41c6a0700ae4f711da902106c9f9c33e35cc4
xuguilins <1352249378@qq.com>
2021-02-19 11:48:00 +0800

    移除不必要的数据

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using DL.Core.EfCore;
+using DL.Core.EfCore.SqlServer;
+using DL.Core.ulitity.ui;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.DependencyInjection;
+using DL.Core.EfCore.engine;
+using DL.Core.ulitity.attubites;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddEnginePack<MyContext>();
+            var service = ServiceLocator.Instace.GetService<IUserService>();
+            service.AddUser();
+            Console.ReadKey();
+        }
+    }
+    public class MyContext : DefaultDbContext<MyContext>
+    {
+        public override string ConnectionString => "Data Source=.;Initial Catalog=Test_T;Integrated Security=True";
+    }
+    public class UserInfo : EntityBase
+    {
+        public string UserName { get; set; }
+    }
+    public class UserInfoConfiguration : ConfigurationBase<UserInfo>
+    {
+        public override Type DbContextType => typeof(MyContext);
+        public override void Configure(EntityTypeBuilder<UserInfo> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.UserName).HasMaxLength(50);
+            builder.Property(m => m.CreatedTime).HasMaxLength(50);
+        }
+    }
+
+    public interface IUserService : IScopeDependcy
+    {
+        void AddUser();
+    }
+    public class UserService : IUserService
+    {
+        private IRepository<UserInfo> _userRespository;
+        public UserService(IRepository<UserInfo> repository)
+        {
+            _userRespository = repository;
+        }
+        public void AddUser()
+        {
+            var abc= _userRespository.AddEntity(new UserInfo
+            { 
+                 UserName = "奥里给"
+            });
+            Console.WriteLine($"输出结果：{abc}");
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,6 +5,7 @@ using DL.Core.Ado;
 using DL.Core.Ado.SqlServer;
 using DL.Core.ulitity;
 using DL.Core.ulitity.attubites;
+using DL.Core.ulitity.ui;
 
 namespace ConsoleApp4
 {
@@ -13,37 +14,13 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
 
-            //手机号脱敏
-            string number = "18720294546";
-            var express = @"^[0-9]*$";
-            var flag = Regex.IsMatch(number, express);
-            int length = number.Length;
-            if (flag && length==11)
-            {
-                var start = number.Substring(0, 3);
-                var middler = "****";
-                var end = number.Substring(7, 4);
-                var res = $"{start}{middler}{end}";
-                Console.WriteLine(res);
-
-
-
-            }
-
-
-
+            
 
             Console.ReadKey();
         }
     }
-    public class UserInfo
+    public class UserInfo:EntityBase
     {
-        [IgnoerColume(true)]
-        public int ID { get; set; }
-        [Description("用户名")]
-        public string USERNAME { get; set; }
-        public int USERAGE { get; set; }
-        public DateTime CREATETIME { get; set; }
-        public string USERPASS { get; set; }
+        public string UserName { get; set; }   
     }
 }
